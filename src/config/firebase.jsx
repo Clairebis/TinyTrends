0f0acef37e1b3efcd62436af57d1a11ef2b1476a
Claire <eaaclhb@students.eaaa.dk>
2023-11-23 23:28:30 +0100

    firebase set up & hosting

---
+++ src/config/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-Hw6JFj8Pr-gT3GaDljGO7vG1D57e--I",
+  authDomain: "gogreenproject-f7bf2.firebaseapp.com",
+  projectId: "gogreenproject-f7bf2",
+  storageBucket: "gogreenproject-f7bf2.appspot.com",
+  messagingSenderId: "1053967386781",
+  appId: "1:1053967386781:web:2889b8e5983740262cac74",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
