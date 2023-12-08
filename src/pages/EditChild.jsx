86e2b3fa423df12ee737c70fafb3dff37f4976e8
Claire <eaaclhb@students.eaaa.dk>
2023-12-09 00:04:34 +0100

    Edit child functionality

--- src/pages/EditChild.jsx
+++ src/pages/EditChild.jsx
@@ -4,7 +4,6 @@ import { getAuth } from "firebase/auth";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { doc, getDoc, updateDoc, collection } from "firebase/firestore";
-import { childrenRef } from "../config/firebase";
 import { db } from "../config/firebase";
 
 export default function EditChild() {
@@ -19,18 +18,26 @@ export default function EditChild() {
     async function getChild() {
       if (auth.currentUser?.uid) {
         try {
-          const docRef = doc(childrenRef, childId);
-          const docSnap = await getDoc(docRef);
+          if (auth.currentUser?.uid) {
+            console.log("User ID:", auth.currentUser.uid);
+            const userChildrenCollectionRef = collection(
+              db,
+              "users",
+              auth.currentUser.uid,
+              "children"
+            );
+            // Get a reference to the specific child based on childId
+            const childDocRef = doc(userChildrenCollectionRef, childId);
 
-          if (docSnap.exists()) {
-            const docData = docSnap.data();
-            if (docData && typeof docData === "object") {
-              setChild(docData); // set child state with the data from the document
+            // Retrieve the child document
+            const childDoc = await getDoc(childDocRef);
+
+            if (childDoc.exists()) {
+              const childData = childDoc.data();
+              setChild(childData);
             } else {
-              console.log("Invalid document data");
+              console.log("Child document does not exist");
             }
-          } else {
-            console.log("Document does not exist");
           }
         } catch (error) {
           console.error("Error fetching child data", error.message);
@@ -47,19 +54,23 @@ export default function EditChild() {
    */
   async function handleSubmit(childToUpdate) {
     if (auth.currentUser?.uid && childId) {
-      // Get a reference to the user's children subcollection
-      const userChildrenCollectionRef = collection(
-        db,
-        "users",
-        auth.currentUser.uid,
-        "children"
-      );
-      // Create a document reference for the specific child based on childId
-      const docRef = doc(userChildrenCollectionRef, childId);
+      try {
+        // Get a reference to the user's children subcollection
+        const userCollectionRef = collection(db, "users");
+        const userDocRef = doc(userCollectionRef, auth.currentUser.uid);
+        const userChildrenCollectionRef = collection(userDocRef, "children");
 
-      // Update the child using the docRef and childToUpdate object
-      await updateDoc(docRef, childToUpdate);
-      navigate(`/home-child-updated/${childId}`); // navigate to success page
+        // Create a document reference for the specific child based on childId
+        const childDocRef = doc(userChildrenCollectionRef, childId);
+
+        // Update the child using the docRef and childToUpdate object
+        await updateDoc(childDocRef, childToUpdate);
+        navigate(`/home-child-updated/${childId}`); // navigate to success page
+      } catch (error) {
+        console.error("Error updating child", error.message);
+      }
+    } else {
+      console.log("Invalid user or childId");
     }
   }
 
