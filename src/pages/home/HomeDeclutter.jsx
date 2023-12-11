ca6bbf3dfc99fd1a3e788c077c54f4da4aa85d41
Claire <eaaclhb@students.eaaa.dk>
2023-12-11 23:08:10 +0100

    declutter styling

--- src/pages/home/HomeDeclutter.jsx
+++ src/pages/home/HomeDeclutter.jsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 import { db } from "../../config/firebase";
 import { doc, collection, query, where, getDocs } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import Donate from "../../assets/Donate.webp";
+import Sell from "../../assets/Sell.webp";
+import Recycle from "../../assets/Recycle.webp";
 
 export default function HomeDeclutter() {
   const { childId } = useParams();
@@ -82,14 +85,53 @@ export default function HomeDeclutter() {
         <p>Loading...</p>
       )}
 
-      <article onClick={handleDonateClick}>
-        <h2>Donate</h2>
+      <p className="declutterIntro1">
+        Your collections. Browse your collections and get tips on how to reuse
+        the items you no longer need.
+      </p>
+
+      <p className="declutterIntro2">
+        Thanks for contributing to sustainable fashion!
+      </p>
+
+      <article onClick={handleDonateClick} className="declutterArticle">
+        <div className="declutterArticleLeft">
+          <div className="declutterArticleImageContainer">
+            <img src={Donate} alt="Donate" />
+          </div>
+        </div>
+        <div className="declutterArticleRight">
+          <p className="declutterArticleText">
+            See the items you’ve marked to donate, and get an overview of our
+            charitable partners’ drop off points.
+          </p>
+        </div>
       </article>
-      <article onClick={handleSellClick}>
-        <h2>Sell</h2>
+      <article onClick={handleSellClick} className="declutterArticle">
+        <div className="declutterArticleLeft">
+          <div className="declutterArticleImageContainer">
+            <img src={Sell} alt="Sell" />
+          </div>
+        </div>
+        <div className="declutterArticleRight">
+          <p className="declutterArticleText">
+            See the items you’ve marked to sell, and find links to platforms
+            where you can list them.
+          </p>
+        </div>
       </article>
-      <article onClick={handleRecycleClick}>
-        <h2>Recycle</h2>
+      <article onClick={handleRecycleClick} className="declutterArticle">
+        <div className="declutterArticleLeft">
+          <div className="declutterArticleImageContainer">
+            <img src={Recycle} alt="Recycle" />
+          </div>
+        </div>
+        <div className="declutterArticleRight">
+          <p className="declutterArticleText">
+            See the items you’ve marked to recycle, and find textile recycling
+            points in your area.
+          </p>
+        </div>
       </article>
     </section>
   );
