84bfacc4b00c7d250d54148591cf396bfe5ee03b
Claire <eaaclhb@students.eaaa.dk>
2023-12-11 12:12:21 +0100

    Child added success styling

--- src/pages/home/HomeChildAdded.jsx
+++ src/pages/home/HomeChildAdded.jsx
@@ -1,17 +1,32 @@
 import { useParams } from "react-router-dom";
 import Button from "../../components/button/Button";
 import "./home.css";
+import Checkmark from "../../assets/icons/checkmark.svg";
+import "./home.css";
 
 export default function HomeItemAdded() {
   const { childId } = useParams();
 
   return (
-    <>
-      <Button
-        text="Start adding to their wardrobe"
-        link={`/home-wardrobe/${childId}`}
-      ></Button>
-      <Button className="buttonSecondary" text="Return home" link="/"></Button>
-    </>
+    <section className="page successPage">
+      <h1>Congratulations!</h1>
+      <div className="successPageContent">
+        <img src={Checkmark} alt="checkmark" />
+        <p>Your child has now been added to your profile.</p>
+        <p>What would you like to do now?</p>
+        <div className="successPageButtons">
+          <Button
+            text="Start adding to their wardrobe"
+            link={`/home-wardrobe/${childId}`}
+            className="successPageButton"
+          ></Button>
+          <Button
+            className="buttonSecondary successPageButton"
+            text="Return home"
+            link="/"
+          ></Button>
+        </div>
+      </div>
+    </section>
   );
 }
