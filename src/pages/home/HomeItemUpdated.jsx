64148579b64e419ce63c17bd72f87d4ed43d3f68
Claire <eaaclhb@students.eaaa.dk>
2023-12-11 12:49:51 +0100

    item updated styles

--- src/pages/home/HomeItemUpdated.jsx
+++ src/pages/home/HomeItemUpdated.jsx
@@ -1,16 +1,31 @@
 import { useParams } from "react-router-dom";
 import Button from "../../components/button/Button";
+import "./home.css";
+import Checkmark from "../../assets/icons/checkmark.svg";
 
 export default function HomeItemUpdated() {
   const { childId } = useParams();
 
   return (
-    <>
-      <Button
-        text="Return to the wardrobe"
-        link={`/home-wardrobe/${childId}`}
-      ></Button>
-      <Button className="buttonSecondary" text="Go home" link="/"></Button>
-    </>
+    <section className="page successPage">
+      <h1>Great!</h1>
+      <div className="successPageContent">
+        <img src={Checkmark} alt="checkmark" />
+        <p>This item has been updated.</p>
+        <p>What would you like to do now?</p>
+        <div className="successPageButtons">
+          <Button
+            text="Return to the wardrobe"
+            link={`/home-wardrobe/${childId}`}
+            className="successPageButton"
+          ></Button>
+          <Button
+            className="buttonSecondary successPageButton"
+            text="Go home"
+            link="/"
+          ></Button>
+        </div>
+      </div>
+    </section>
   );
 }
