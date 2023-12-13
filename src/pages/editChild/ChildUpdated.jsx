b19423b6316b19c37bd29fb3628cfad4d534ae59
Claire <eaaclhb@students.eaaa.dk>
2023-12-13 22:05:49 +0100

    childUpdated page

--- src/pages/editChild/ChildUpdated.jsx
+++ src/pages/editChild/ChildUpdated.jsx
@@ -1,15 +1,29 @@
 import Button from "../../components/button/Button";
 import { useParams } from "react-router-dom";
+import "./editChild.css";
+import Checkmark from "../../assets/icons/Checkmark.svg";
 
 export default function ChildUpdated() {
   const { childId } = useParams();
+
   return (
-    <section className="page">
+    <section className="page childDeletedPage">
+      <h1 className="deletedChildHeading">Great!</h1>
+      <img src={Checkmark} alt="Checkmark" className="childDeletedCheckmark" />
+      <p className="childUpdatedPara1">
+        This child's information has been updated.
+      </p>
+      <p className="childUpdatedPara2">What would you like to do now?</p>
       <Button
-        text="Return to the wardrobe"
+        className="childUpdatedButton"
+        text="Go to their wardrobe"
         link={`/home-wardrobe/${childId}`}
       ></Button>
-      <Button className="buttonSecondary" text="Go home" link="/"></Button>
+      <Button
+        className="buttonSecondary childUpdatedButton"
+        text="Go home"
+        link="/"
+      ></Button>
     </section>
   );
 }
