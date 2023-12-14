771236e9fbbba22d317213d34076b37480ecf3e3
Claire <eaaclhb@students.eaaa.dk>
2023-12-14 10:25:22 +0100

    signup

--- src/pages/authentication/Signup.jsx
+++ src/pages/authentication/Signup.jsx
@@ -4,23 +4,27 @@ import { Link } from "react-router-dom";
 import { doc, setDoc, collection, addDoc } from "@firebase/firestore";
 import { usersRef } from "../../config/firebase";
 import { db } from "../../config/firebase";
+import "./authentication.css";
+import TextField from "@mui/material/TextField";
+import Hanger from "../../assets/Hanger.webp";
 
 export default function Signup() {
   const [name, setName] = useState("");
-  const [mail, setMail] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); // password state
   const [errorMessage, setErrorMessage] = useState("");
 
   const auth = getAuth();
 
   async function handleSignUp(event) {
     event.preventDefault();
-    const mail = event.target.mail.value; // mail value from input field in sign in form
+    const email = event.target.email.value; // mail value from input field in sign in form
     const password = event.target.password.value; // password value from input field in sign in form
 
     try {
       const userCredential = await createUserWithEmailAndPassword(
         auth,
-        mail,
+        email,
         password
       );
       const user = userCredential.user;
@@ -31,7 +35,7 @@ export default function Signup() {
       // Set the user in Firestore with the values from input fields
       await setDoc(userDocRef, {
         name,
-        mail,
+        email,
         itemsDonated: 0,
         itemsSold: 0,
         itemsRecycled: 0,
@@ -56,34 +60,57 @@ export default function Signup() {
   }
 
   return (
-    <section className="page">
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSignUp}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          name="name"
-          placeholder="Type your name"
-        />
-        <input
-          type="email"
-          value={mail}
-          onChange={(e) => setMail(e.target.value)}
-          name="mail"
-          placeholder="Type your mail"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Type your password"
-        />
+    <section className="page signupPage">
+      <img src={Hanger} alt="Hanger" className="loginHanger" />
+      <h1 className="landingTitle">TinyTrends</h1>
+      <form onSubmit={handleSignUp} className="signupForm">
+        <div className="loginInputContainer">
+          <TextField
+            className="loginInput"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            name="name"
+            placeholder="Type your name"
+            required
+          />
+        </div>
+
+        <div className="loginInputContainer">
+          <TextField
+            className="loginInput"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            placeholder="Type your mail"
+            required
+          />
+        </div>
+
+        <div className="loginInputContainer">
+          <TextField
+            className="loginInput"
+            type="password"
+            name="password"
+            placeholder="Type your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
         <p className="text-error">{errorMessage}</p>
-        <button>Sign Up</button>
+        <button className="signupButton buttonForestGreen">Sign Up</button>
       </form>
-      <p className="text-center">
-        Already have an account? <Link to="/login">Log In</Link>
-      </p>
+
+      <div className="loginSignupLink">
+        <p>
+          Already have an account?{" "}
+          <Link to="/login" className="signUpLink">
+            Log In
+          </Link>
+        </p>
+      </div>
     </section>
   );
 }
