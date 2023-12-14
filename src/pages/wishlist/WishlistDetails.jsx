5d5f86634620c25733d365186d83731a3f1d6819
Claire <eaaclhb@students.eaaa.dk>
2023-12-14 22:59:13 +0100

    wishlist details page

--- src/pages/wishlist/WishlistDetails.jsx
+++ src/pages/wishlist/WishlistDetails.jsx
@@ -1,8 +1,31 @@
 import { useParams } from "react-router-dom";
+import Button from "../../components/button/Button";
+import Hanger from "../../assets/hanger.webp";
 
 export default function WishlistDetails() {
-  const listId = useParams().listId;
+  const listId = useParams();
   console.log("listId:", listId);
 
-  return <div>HomeListDetails</div>;
+  return (
+    <>
+      <section className="page wishlistDetailsPage">
+        <div className="wishlistContentContainer">
+          <h1>My Lists</h1>
+        </div>
+        <div className="hangerImageContainer">
+          <img src={Hanger} alt="Hanger logo" className="wishlistHangerImage" />
+        </div>
+        <h2>What do you need?</h2>
+        <p className="wishlistPara">
+          Tap the button below to start adding items.
+        </p>
+      </section>
+
+      <Button
+        text="+ ADD ITEMS"
+        className="wishlistAddButton"
+        link="/wishlist-add-list"
+      />
+    </>
+  );
 }
